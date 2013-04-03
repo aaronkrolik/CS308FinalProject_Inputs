4d4261ecf1dd69666570cd93e5ccba8ce985ab41
Gavin Ovsak <ovsak.gavin@gmail.com>
2013-04-02 21:08:34 -0400

    Cleaned up keyboard listener

--- src/input/KeyboardListener.java
+++ src/input/KeyboardListener.java
@@ -2,21 +2,17 @@ package input;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.HashMap;
-
 import javax.swing.JComponent;
 
 public class KeyboardListener extends InputDevice{
 	JComponent myComponent;
 	private Input myInput;
-	//private KeyboardInitialler initialler;
 	public final String myDevice = "KEYBOARD";
 	
 	public KeyboardListener(JComponent component, Input input){
 		super("Keyboard");
 		myComponent = component;
 		myInput = input;
-		//initialler = new KeyboardInitialler();
 		initialize();
 	}
 	
@@ -31,33 +27,19 @@ public class KeyboardListener extends InputDevice{
 		myComponent.addKeyListener(new KeyListener() {
 			@Override
 			public void keyPressed(KeyEvent e){
-				String action = getKeyPressed(e.getKeyCode());
-				myInput.actionNotify(action, getObject(e));
+				String keyName = KeyboardHelper.getKeyName(e.getKeyCode());
+				myInput.actionNotify("Keyboard_" + keyName + "_KeyDown", getObject(e));
 			}
 
 			@Override
 			public void keyReleased(KeyEvent e) {
-				String action = getKeyReleased(e.getKeyCode());
-				myInput.actionNotify(action, getObject(e));
+				String keyName = KeyboardHelper.getKeyName(e.getKeyCode());
+				myInput.actionNotify("Keyboard_" + keyName + "_KeyUp", getObject(e));
 			}
 
 			@Override
 			public void keyTyped(KeyEvent e) {				
 			}
 		});
-	}
-	
-	private String getKeyPressed(int key){
-		String thirdIdentifier = "" + ((char)key);
-		if(key == KeyEvent.VK_SPACE)
-			thirdIdentifier = "Spacebar";
-		return "Keyboard_" + thirdIdentifier + "_KeyDown";
-	}
-	
-	private String getKeyReleased(int key){
-		String thirdIdentifier = "" + (char)key;
-		if(key == KeyEvent.VK_SPACE)
-			thirdIdentifier = "Spacebar";
-		return "Keyboard_" + thirdIdentifier + "_KeyUp";
 	}
 }
