2fa97ac3597bb5fe3eb91f915e0557624004308e
Aaron Krolik <Aaron.B.Krolik@Gmail.com>
2013-04-04 01:45:43 -0400

    added comments

--- src/input/Input.java
+++ src/input/Input.java
@@ -19,70 +19,100 @@ import java.util.ResourceBundle;
 import javax.swing.JComponent;
 
 import examples.Player;
+
 /**
- * Singleton Input class to handle all sorts of input goodies behind the hood 
+ * Singleton Input class to handle all sorts of input goodies behind the hood
+ * 
  * @author aaronkrolik
- *
+ * 
  */
 public class Input {
-	
+
 	private static Input myInstance;
-	
+
 	private final ResourceBundle RESOURCES;
-	
-	HashMap<String, Command> behaviors = new HashMap<String, Command>();
+
 	List<WeakReference> myWeakReferences = new ArrayList<WeakReference>();
 	Map<String, Method> keyToMethod = new HashMap<String, Method>();
 	Map<String, Object> keyToInstance = new HashMap<String, Object>();
-	ArrayList<InputDevice> inputDevices = new ArrayList<InputDevice>();
+	ArrayList<InputDevice> inputDevices = new ArrayList<InputDevice>(); //still not sure if needed 
 
-
-	
-	public static Input newSingletonInput(String resourcePath, JComponent component){
+	/**
+	 * (Convenience) Singleton constructor. I'm almost as ashamed as I am lazy
+	 * 
+	 * @param String shameful
+	 * @param JComponent hack
+	 * @return Input instance
+	 */
+	public static Input newSingletonInput(String resourcePath,
+			JComponent component) {
 		if (myInstance == null) {
 			myInstance = new Input(resourcePath, component);
 		}
 		return myInstance;
 	}
-	
-	public static Input getSingeltonInput(){
+
+	public static Input getSingeltonInput() {
 		return myInstance;
 	}
-	
+
 	private Input(String resourcePath, JComponent component) {
 		RESOURCES = ResourceBundle.getBundle(resourcePath);
 		inputDevices.add(new KeyboardModule(component));
 		inputDevices.add(new MouseModule(component));
 	}
-	
-	//TODO handle overloaded methods. need incorporate the param field
-	public void addListenerTo(Object in){
+
+	/**
+	 * Include input instance in our collection of instances that can have
+	 * annotated methods invoked
+	 * 
+	 * @param Object input. any object will do 
+	 * TODO handle overloaded methods. need incorporate the param field
+	 *            
+	 */
+	public void addListenerTo(Object in) {
 		myWeakReferences.add(new WeakReference(in));
-		Class inputClass = in.getClass();	
-		if (inputClass.getAnnotation(InputClassTarget.class) != null){
-			for (Method method : inputClass.getMethods()){
-				Annotation annotation = method.getAnnotation(InputMethodTarget.class);
-				if (annotation instanceof InputMethodTarget){
-					System.out.println("reflecting"+((InputMethodTarget) annotation).name());
-					keyToMethod.put(((InputMethodTarget) annotation).name(), method);
-				}	
+		Class inputClass = in.getClass();
+		if (inputClass.getAnnotation(InputClassTarget.class) != null) {
+			for (Method method : inputClass.getMethods()) {
+				Annotation annotation = method
+						.getAnnotation(InputMethodTarget.class);
+				if (annotation instanceof InputMethodTarget) {
+					System.out.println("reflecting"
+							+ ((InputMethodTarget) annotation).name());
+					keyToMethod.put(((InputMethodTarget) annotation).name(),
+							method);
+				}
 			}
-		}			
+		}
 	}
-	
-	public void removeListener(Object in){
-		for (int i=0; i<myWeakReferences.size(); i++){
-			if (in == myWeakReferences.get(i).get()){
+
+	/**
+	 * Remove an instance from our cache. This means that it will not be called
+	 * even if it has relevant target
+	 * 
+	 * @param Object input (any type will do)
+	 */
+	public void removeListener(Object in) {
+		for (int i = 0; i < myWeakReferences.size(); i++) {
+			if (in == myWeakReferences.get(i).get()) {
 				myWeakReferences.remove(i);
 				System.out.println("out!");
 				break;
 			}
 		}
 	}
-	
-	//TODO get working with the methodInputs
-	public void execute(String key, ActionObject in){
-		for (WeakReference x : myWeakReferences){
+
+	/**
+	 * Uses nifty methodObject and instance caches to reflexively invoke methods
+	 * TODO: get working with the methodInputs 
+	 * TODO: real exceptions ...
+	 * 
+	 * @param key
+	 * @param in
+	 */
+	public void execute(String key, ActionObject in) {
+		for (WeakReference x : myWeakReferences) {
 			try {
 				keyToMethod.get(key).invoke(x.get(), null);
 			} catch (IllegalArgumentException e) {
@@ -94,26 +124,25 @@ public class Input {
 			} catch (InvocationTargetException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			} catch (NullPointerException e){
-				
+			} catch (NullPointerException e) {
+
 			}
 		}
 	}
-	
-	
-	public void setBehavior(String behaviorName, Command behavior) {
-		behaviors.put(behaviorName, behavior);
-	}
-	
-	public void actionNotification(String action, ActionObject object){
-		try{
-		execute(RESOURCES.getString(action), object);
-		}catch (NullPointerException e){
+
+	/**
+	 * actionNotification aka massiveHack.
+	 * TODO: make less shitty.
+	 * 
+	 * @param action
+	 * @param object
+	 *        
+	 */
+	public void actionNotification(String action, ActionObject object) {
+		try {
+			execute(RESOURCES.getString(action), object);
+		} catch (NullPointerException e) {
 			System.out.println("null pointer oops");
-		}
-		if(RESOURCES.containsKey(action) && behaviors.containsKey(RESOURCES.getString(action))) {
-			//behaviors.get(RESOURCES.getString(action)).execute(object);
-			
 		}
 	}
 }
